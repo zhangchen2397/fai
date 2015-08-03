316b0ec7cb62f0ef778850213513002403d9f216
zhangchen <zhangchen2397@126.com>
2015-08-03 20:53:42 +0800

    up by zc

--- fai.js
+++ fai.js
@@ -2,17 +2,3 @@ var fis = module.exports = require('fis');
 
 fis.cli.name = 'fai';
 fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
-
-// fis.config.merge({
-//     roadmap : {
-//         ext : {
-//             md : 'html'
-//         }
-//     },
-//     modules : {
-//         parser : {
-//             md : 'marked'
-//         }
-//     }
-// });
-
